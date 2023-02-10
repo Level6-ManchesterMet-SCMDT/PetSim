be7ff000cd82bef517151f7ebd54bac3c56065af
howardxu23 <xu.howard@hotmail.com>
2023-02-10 16:01:00 +0000

    Update AnimalParentScript.cs

--- zoo care simulator/Assets/scripts/AnimalParentScript.cs
+++ zoo care simulator/Assets/scripts/AnimalParentScript.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class AnimalParentScript : MonoBehaviour
 {
+    public string animalName;
+    public int maxValue;
+    public int health = 50;
+    public int mood = 50;
+    public int hunger=50;
+    public float growthRate;
+    public float age;
+    [SerializeField]
+    [Tooltip("aliments for matching with the correct medicene")]
+    protected string[] alimentsList ={"flu","rash"};
+    public string CurrentAliment;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +25,21 @@ public class AnimalParentScript : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Feed(int foodvalue=10)//increases hunger bar from feeding
+    {
+        hunger += foodvalue;
+    }
+    public void GiveMedicine(string medicine)//input what the medicene cures and it increases health
+    {
+        if (medicine == CurrentAliment)
+        {
+            CurrentAliment = "healthy";
+            health += 10;
+        }
+        else
+        {
+            health -= 10;
+        }
     }
 }
