119770ea439a508484a4ef24edd8d6a2702d5030
Ryan Hughes <Ryan.hughes31@hotmail.com>
2023-02-21 21:40:20 +0000

    Added 'H' to open and close menu feature
    Pressing H opens and closes the handbook. The camera and movement is frozen and cursor lock is disabled whenever the handbook is open.

---
+++ zoo care simulator/Assets/HandbookController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class HandbookController : MonoBehaviour
+{
+    [SerializeField] private GameObject handbookCanvas;
+    [SerializeField] private FirstPersonController fpsScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handbookCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OpenHandbook();
+    }
+
+    private void OpenHandbook()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            handbookCanvas.SetActive(!handbookCanvas.activeInHierarchy);
+            fpsScript.m_MouseLook.SetCursorLock(!handbookCanvas.activeInHierarchy);
+            GetComponent<FirstPersonController>().enabled = !handbookCanvas.activeInHierarchy;
+            
+
+        }
+    }
+}
