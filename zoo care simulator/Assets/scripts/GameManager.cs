16a4f7ecf8e69b53dd10add242535fcd66223dfb
WFMcDouall <wmcdouall@yahoo.com>
2023-02-23 10:33:22 +0000

    - Added textures and scoring

--- zoo care simulator/Assets/scripts/GameManager.cs
+++ zoo care simulator/Assets/scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,10 +13,20 @@ public class GameManager : MonoBehaviour
     public int currentScore;
     public int scorePerNote = 100;
     
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+  
+    public Text scoreText;
+    public Text multiText;
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+
+        scoreText.text = "Score: 0";
+        currentMultiplier = 1;
     }
 
     // Update is called once per frame
@@ -36,12 +47,31 @@ public class GameManager : MonoBehaviour
     public void NoteHit()
     {
         Debug.Log("Hit on time!");
+        currentScore += scorePerNote * currentMultiplier;
 
-        currentScore += scorePerNote;
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] <= multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
+
+        scoreText.text = "Score: " + currentScore;
     }
 
     public void NoteMissed()
     {
         Debug.Log("Missed note...");
+
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+
+        multiText.text = "Multiplier: x" + currentMultiplier;
     }
 }
