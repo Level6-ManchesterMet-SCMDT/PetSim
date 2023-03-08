27fc069b048c709c1c904a3c6caaefdbd6579580
Ryan Hughes <Ryan.hughes31@hotmail.com>
2023-03-08 21:39:40 +0000

    PC Basics complete
    PC can be interacted with. The Camera changes and you zoom into the screen.
    Startup and shutdown music plays. Brief bootup screen using Callums animation.
    
    Future Additions:
    -Virtual Cursor
    -Log out button
    - Zoom out when closed?

--- zoo care simulator/Assets/scripts/pcController.cs
+++ zoo care simulator/Assets/scripts/pcController.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class pcController : MonoBehaviour
@@ -12,23 +15,23 @@ public class pcController : MonoBehaviour
     [SerializeField] private GameObject prompt;
     [SerializeField] private GameObject playerCvs;
     [SerializeField] private GameObject screenSaver;
+    [SerializeField] private GameObject bootUp;
     [SerializeField] private GameObject desktop;
+    [SerializeField] private AudioSource startup, shutdown;
+    private Coroutine zoomCoroutine;
+    private float targetFOV = 20, startFOV = 60;
+    private float counter = 0;
     
     // Start is called before the first frame update
     void Start()
     {
         prompt.SetActive(false);
         screenSaver.SetActive(true);
+        bootUp.SetActive(false);
         desktop.SetActive(false);
         pcCam.GetComponent<Camera>().enabled = false;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnTriggerEnter(Collider other)
     {
         prompt.SetActive(true);
@@ -43,8 +46,9 @@ public class pcController : MonoBehaviour
     {
         if (collider.gameObject.CompareTag("Player"))
         {
-            if (Input.GetKey(KeyCode.E))
+            if (Input.GetKey(KeyCode.E)) //Enter the PC
             {
+                counter = 0;
                 playerCvs.SetActive(false);
                 player.GetComponent<FirstPersonController>().m_MouseLook.SetCursorLock(false);
                 player.GetComponent<FirstPersonController>().enabled = false;
@@ -52,11 +56,19 @@ public class pcController : MonoBehaviour
                 player.GetComponentInChildren<Camera>().enabled = false;
                 pcCam.GetComponent<Camera>().enabled = true;
                 screenSaver.SetActive(false);
-                desktop.SetActive(true);
+                startup.Play();
+                bootUp.SetActive(true);
                 prompt.SetActive(false);
+                if (zoomCoroutine != null)
+                    StopCoroutine(zoomCoroutine);
+                zoomCoroutine = StartCoroutine(ZoomIn(20.5f,1));
+               
             }
-            else if (Input.GetKey(KeyCode.R))
+            else if (Input.GetKey(KeyCode.R)) //Exit the pc -- will be a log out button in future
             {
+                if (zoomCoroutine != null)
+                    StopCoroutine(zoomCoroutine);
+                pcCam.GetComponent<Camera>().fieldOfView = 60;
                 playerCvs.SetActive(true);
                 player.GetComponent<FirstPersonController>().enabled = true;
                 player.GetComponent<FirstPersonController>().m_MouseLook.SetCursorLock(true);
@@ -64,6 +76,7 @@ public class pcController : MonoBehaviour
                 player.GetComponentInChildren<Camera>().enabled = true;
                 pcCam.GetComponent<Camera>().enabled = false;
                 desktop.SetActive(false);
+                shutdown.Play();
                 screenSaver.SetActive(true);
                 prompt.SetActive(true);
             }
@@ -71,6 +84,48 @@ public class pcController : MonoBehaviour
         }
         
     }
-    
-    
+
+    IEnumerator ZoomIn(float target, float duration) //https://stackoverflow.com/questions/47957484/zoom-camera-fov-overtime
+    {
+        while (pcCam.GetComponent<Camera>().fieldOfView>target)
+        {
+            counter += Time.deltaTime;
+            float fOVTime = counter / duration;
+            Debug.Log(fOVTime);
+
+            //Change FOV
+            pcCam.GetComponent<Camera>().fieldOfView = Mathf.Lerp(startFOV, targetFOV, fOVTime);
+            if (fOVTime > 0.95)
+            {
+                bootUp.SetActive(false);
+                desktop.SetActive(true);
+            }
+            //Wait for a frame
+            yield return null;
+        }
+    }
+
+    public void MeerkatButton()
+    {
+        DontDestroyOnLoad(player);
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.LoadScene("FeedMeerkat2DMiniGame");
+    }
+    public void PandaButton()
+    {
+        Debug.Log("PANDA GAME STARTED");
+    }
+    public void PenguinButton()
+    {
+        Debug.Log("PENGUIN GAME STARTED");
+    }
+    public void SlothButton()
+    {
+        Debug.Log("SLOTH GAME STARTED");
+    }
+    public void CoatiButton()
+    {
+        Debug.Log("COATI GAME STARTED");
+    }
+
 }
