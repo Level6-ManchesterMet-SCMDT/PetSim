3d62d31cb14ea827766db69fc26d0c8751b7f856
Ryan Hughes <Ryan.hughes31@hotmail.com>
2023-03-08 17:48:28 +0000

    Player can sit in front of PC and interact.
    Using a second camera, the player can press E to sit at the computer and use the buttons on the desktop.
    Need to add FOV change over time

--- zoo care simulator/Assets/pcController.cs
+++ zoo care simulator/Assets/pcController.cs
@@ -10,11 +10,17 @@ public class pcController : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject pcCam;
     [SerializeField] private GameObject prompt;
+    [SerializeField] private GameObject playerCvs;
+    [SerializeField] private GameObject screenSaver;
+    [SerializeField] private GameObject desktop;
     
     // Start is called before the first frame update
     void Start()
     {
         prompt.SetActive(false);
+        screenSaver.SetActive(true);
+        desktop.SetActive(false);
+        pcCam.GetComponent<Camera>().enabled = false;
     }
 
     // Update is called once per frame
@@ -39,20 +45,26 @@ public class pcController : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.E))
             {
+                playerCvs.SetActive(false);
                 player.GetComponent<FirstPersonController>().m_MouseLook.SetCursorLock(false);
                 player.GetComponent<FirstPersonController>().enabled = false;
                 player.GetComponent<ZoologistControl>().enabled = false;
                 player.GetComponentInChildren<Camera>().enabled = false;
                 pcCam.GetComponent<Camera>().enabled = true;
+                screenSaver.SetActive(false);
+                desktop.SetActive(true);
                 prompt.SetActive(false);
             }
             else if (Input.GetKey(KeyCode.R))
             {
+                playerCvs.SetActive(true);
                 player.GetComponent<FirstPersonController>().enabled = true;
                 player.GetComponent<FirstPersonController>().m_MouseLook.SetCursorLock(true);
                 player.GetComponent<ZoologistControl>().enabled = true;
                 player.GetComponentInChildren<Camera>().enabled = true;
                 pcCam.GetComponent<Camera>().enabled = false;
+                desktop.SetActive(false);
+                screenSaver.SetActive(true);
                 prompt.SetActive(true);
             }
 
