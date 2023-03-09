d1ecc8becfe448da02622f2665bbe9cbfad8d875
Ryan Hughes <Ryan.hughes31@hotmail.com>
2023-03-09 03:22:50 +0000

    Blackboard Customisation Added
    - Scene Setup
    - Each blackboard can now be customised for each individual animal. With a dropdown for animal name, an array to add tasks and cloning mechanic.
    
    ToDo:
    - Add code for task completion

--- zoo care simulator/Assets/scripts/BlackboardPrompt.cs
+++ zoo care simulator/Assets/scripts/BlackboardPrompt.cs
@@ -1,23 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Toggle = UnityEngine.UI.Toggle;
 
 public class BlackboardPrompt : MonoBehaviour
 {
+    [Header("Setup")]
     [SerializeField] private GameObject prompt;
+    [SerializeField] private GameObject taskPrefab;
     [SerializeField] private Text taskText;
     [SerializeField] private Toggle taskCompletion;
-
-    private string[] tasks= new string[] { "Feed the Penguins", "Clean Penguin Enclosure", "Treat Unwell Penguins", "Set Penguin Thermostat", "Clock Out"};
+    [SerializeField] private TMP_Text BlackboardTitle;
+    
+    [Header("Animal Details")]
+    [SerializeField]private string[] tasks;
+    public enum Species { Penguin, Panda, Meerkat, Coati, Sloth };
+    public Species _species;
+    
     private bool inTrigger = false;
     private int nextTask = 1;
     private int completed = 0;
-
+    
     private void Start()
     {
-        taskText.text = tasks[0];
+        BlackboardTitle.fontSize = 60;
+        BlackboardTitle.text = _species + " Tasks";
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            if (i<1)
+            {taskText.text = tasks[i];}
+            else
+            {
+                Toggle ToggleClone = Instantiate(taskCompletion, new Vector3(taskCompletion.transform.position.x,taskCompletion.transform.position.y,
+                    taskCompletion.transform.position.z), taskCompletion.transform.rotation);
+                ToggleClone.transform.parent = taskCompletion.transform.parent;
+                ToggleClone.transform.localScale = taskCompletion.transform.localScale;
+                ToggleClone.GetComponentInChildren<Text>().text = tasks[i];
+            }
+        }
         Debug.Log(completed + "/" + tasks.Length + " tasks completed");
     }
 
