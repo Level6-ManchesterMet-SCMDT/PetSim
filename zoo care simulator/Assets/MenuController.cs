9d2410f3113eaac386e678be433d5202d2e30444
Ryan Hughes <Ryan.hughes31@hotmail.com>
2023-03-16 23:10:21 +0000

    Added Animations up to the second sign

--- zoo care simulator/Assets/MenuController.cs
+++ zoo care simulator/Assets/MenuController.cs
@@ -5,19 +5,41 @@ using UnityEngine;
 public class MenuController : MonoBehaviour
 {
     [SerializeField] private Animator Anim;
+    [SerializeField] private GameObject firstPrompt;
+    [SerializeField] private KeyCode PressToPlay;
+    [SerializeField] private GameObject FirstMenu;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        firstPrompt.SetActive(false);
+        FirstMenu.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(PressToPlay))
         {
             Anim.SetTrigger("GameStart");
+            firstPrompt.SetActive(false);
+            
         }  
+    }
+    
+    public void AlertObservers(string message)
+    {
+        if (message.Equals("IntroAnimationEnded"))
+        {
+            firstPrompt.SetActive(true);
+        }
+    }
+
+    public void ShowFirstMenu(string message)
+    {
+        if (message.Equals("TravelAnimationEnded"))
+        {
+            FirstMenu.SetActive(true);
+        }
     }
 }
