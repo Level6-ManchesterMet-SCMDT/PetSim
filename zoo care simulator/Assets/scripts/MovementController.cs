075880248a64609efd013098218947e939c54c93
WFMcDouall <wmcdouall@yahoo.com>
2023-03-23 11:52:28 +0000

    Signed-off-by: WFMcDouall <wmcdouall@yahoo.com>

---
+++ zoo care simulator/Assets/scripts/MovementController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public GameObject currentNode;
+    public float speed = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        NodeController currentNodeController = currentNode.GetComponent<NodeController>();
+        transform.position = Vector2.MoveTowards(transform.position, currentNode.transform.position, speed * Time.deltaTime);
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+   
+        rb.velocity = new Vector3(x * 15.0f, y * 15.0f, 0);
+    }
+}
