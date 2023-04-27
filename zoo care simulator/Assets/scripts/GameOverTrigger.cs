a54bb3db657198949289f2991cbef84358f975ad
WFMcDouall <wmcdouall@yahoo.com>
2023-04-27 14:24:14 +0100

    Added Game Over Screen

--- zoo care simulator/Assets/scripts/GameOverTrigger.cs
+++ zoo care simulator/Assets/scripts/GameOverTrigger.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameOverTrigger : MonoBehaviour
 {
+    public GameObject loseScreen;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("You Lose!");
+            loseScreen.SetActive(true);
+            Destroy(gameObject); 
+        }
     }
 }
