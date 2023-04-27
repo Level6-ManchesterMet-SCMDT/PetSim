8fe130515d6b3f74180ba64087536ed909e62453
Ryan Hughes <ryan.hughes31@hotmail.com>
2023-04-27 11:48:18 +0100

    More helpful code

--- zoo care simulator/Assets/scripts/StatTracker.cs
+++ zoo care simulator/Assets/scripts/StatTracker.cs
@@ -11,73 +11,59 @@ using Slider = UnityEngine.UI.Slider;
 public class StatTracker : MonoBehaviour
 {
     [Header("Setup")]
-    [SerializeField] private Sprite[] Icons;
-    [SerializeField] private Text[] Names;
+    [SerializeField, Tooltip("Drag in all animal pixel art icon")] private Sprite[] Icons;
+    [SerializeField, Tooltip("Drag in all the animal name object from each entry")] private Text[] Names;
     [SerializeField] private Gradient gradient;
 
     [Header("Penguin Variables")]
-    [SerializeField] private GameObject PenguinEntry;
-    [SerializeField] private Image PenguinPic;
-    [SerializeField] private TMP_Text PenguinName;
-    [SerializeField] private TMP_Text PenguinHealth;
-    [SerializeField] private TMP_Text PenguinHunger;
-    [SerializeField] private TMP_Text PenguinHappy;
-    [SerializeField] private Slider PenguinOverall;
-    [SerializeField] private Image PenguinFill;
-    [SerializeField] private TMP_Text PenguinPriority;
-    [SerializeField] private Slider[] PenguinCurrentStats;
+    [SerializeField, Tooltip("Drag in image object from first data entry")] private Image PenguinPic;
+    [SerializeField, Tooltip("Drag in text object that's underlined from first data entry")] private TMP_Text PenguinName;
+    [SerializeField, Tooltip("Drag in text object that's next to the heart from first data entry")] private TMP_Text PenguinHealth;
+    [SerializeField, Tooltip("Drag in text object that's next to the stomach from first data entry")] private TMP_Text PenguinHunger;
+    [SerializeField, Tooltip("Drag in text object that's next to the smiley from first data entry")] private TMP_Text PenguinHappy;
+    [SerializeField, Tooltip("Drag in slider object from first data entry")] private Slider PenguinOverall;
+    [SerializeField, Tooltip("Drag in sliderfill object from first data entry to apply gradient")] private Image PenguinFill;
+    [SerializeField, Tooltip("Drag in all sliders object from penguin")] private Slider[] PenguinCurrentStats;
     
     [Header("Panda Variables")] 
-    [SerializeField] private GameObject PandaEntry;
-    [SerializeField] private Image PandaPic;
-    [SerializeField] private TMP_Text PandaName;
-    [SerializeField] private TMP_Text PandaHealth;
-    [SerializeField] private TMP_Text PandaHunger;
-    [SerializeField] private TMP_Text PandaHappy;
-    [SerializeField] private Slider PandaOverall;
-    [SerializeField] private TMP_Text PandaPriority;
-    [SerializeField] private Image PandaFill;
-    [SerializeField] private Slider[] PandaCurrentStats;
+    [SerializeField, Tooltip("Drag in image object from second data entry")] private Image PandaPic;
+    [SerializeField, Tooltip("Drag in text object that's underlined from second data entry")] private TMP_Text PandaName;
+    [SerializeField, Tooltip("Drag in text object that's next to the heart from second data entry")] private TMP_Text PandaHealth;
+    [SerializeField, Tooltip("Drag in text object that's next to the stomach from second data entry")] private TMP_Text PandaHunger;
+    [SerializeField, Tooltip("Drag in text object that's next to the smiley from second data entry")] private TMP_Text PandaHappy;
+    [SerializeField, Tooltip("Drag in slider object from second data entry")] private Slider PandaOverall;
+    [SerializeField, Tooltip("Drag in sliderfill object from second data entry to apply gradient")] private Image PandaFill;
+    [SerializeField, Tooltip("Drag in all sliders object from Panda")] private Slider[] PandaCurrentStats;
     
     [Header("Coati Variables")] 
-    [SerializeField] private GameObject CoatiEntry;
-    [SerializeField] private Image CoatiPic;
-    [SerializeField] private TMP_Text CoatiName;
-    [SerializeField] private TMP_Text CoatiHealth;
-    [SerializeField] private TMP_Text CoatiHunger;
-    [SerializeField] private TMP_Text CoatiHappy;
-    [SerializeField] private Slider CoatiOverall;
-    [SerializeField] private TMP_Text CoatiPriority;
-    [SerializeField] private Image CoatiFill;
-    [SerializeField] private Slider[] CoatiCurrentStats;
+    [SerializeField, Tooltip("Drag in image object from third data entry")] private Image CoatiPic;
+    [SerializeField, Tooltip("Drag in text object that's underlined from third data entry")] private TMP_Text CoatiName;
+    [SerializeField, Tooltip("Drag in text object that's next to the heart from third data entry")] private TMP_Text CoatiHealth;
+    [SerializeField, Tooltip("Drag in text object that's next to the stomach from third data entry")] private TMP_Text CoatiHunger;
+    [SerializeField, Tooltip("Drag in text object that's next to the smiley from third data entry")] private TMP_Text CoatiHappy;
+    [SerializeField, Tooltip("Drag in slider object from third data entry")] private Slider CoatiOverall;
+    [SerializeField, Tooltip("Drag in sliderfill object from third data entry to apply gradient")] private Image CoatiFill;
+    [SerializeField, Tooltip("Drag in all sliders object from Coati")] private Slider[] CoatiCurrentStats;
     
     [Header("Meerkat Variables")] 
-    [SerializeField] private GameObject MeerkatEntry;
-    [SerializeField] private Image MeerkatPic;
-    [SerializeField] private TMP_Text MeerkatName;
-    [SerializeField] private TMP_Text MeerkatHealth;
-    [SerializeField] private TMP_Text MeerkatHunger;
-    [SerializeField] private TMP_Text MeerkatHappy;
-    [SerializeField] private Slider MeerkatOverall;
-    [SerializeField] private TMP_Text MeerkatPriority;
-    [SerializeField] private Image MeerkatFill;
-    [SerializeField] private Slider[] MeerkatCurrentStats;
+    [SerializeField, Tooltip("Drag in image object from fourth data entry")] private Image MeerkatPic;
+    [SerializeField, Tooltip("Drag in text object that's underlined from fourth data entry")] private TMP_Text MeerkatName;
+    [SerializeField, Tooltip("Drag in text object that's next to the heart from fourth data entry")] private TMP_Text MeerkatHealth;
+    [SerializeField, Tooltip("Drag in text object that's next to the stomach from fourth data entry")] private TMP_Text MeerkatHunger;
+    [SerializeField, Tooltip("Drag in text object that's next to the smiley from fourth data entry")] private TMP_Text MeerkatHappy;
+    [SerializeField, Tooltip("Drag in slider object from fourth data entry")] private Slider MeerkatOverall;
+    [SerializeField, Tooltip("Drag in sliderfill object from fourth data entry to apply gradient")] private Image MeerkatFill;
+    [SerializeField, Tooltip("Drag in all sliders object from Meerkat")] private Slider[] MeerkatCurrentStats;
     
     [Header("Sloth Variables")] 
-    [SerializeField] private GameObject SlothEntry;
-    [SerializeField] private Image SlothPic;
-    [SerializeField] private TMP_Text SlothName;
-    [SerializeField] private TMP_Text SlothHealth;
-    [SerializeField] private TMP_Text SlothHunger;
-    [SerializeField] private TMP_Text SlothHappy;
-    [SerializeField] private Slider SlothOverall;
-    [SerializeField] private TMP_Text SlothPriority;
-    [SerializeField] private Image SlothFill;
-    [SerializeField] private Slider[] SlothCurrentStats;
-
-    private List<float> animalAverage = new List<float> { 0, 0, 0, 0, 0 };
-    private List<GameObject> AnimalEntryList = new List<GameObject>(5);
-    private List<TMP_Text> PriorityList = new List<TMP_Text>(5);
+    [SerializeField, Tooltip("Drag in image object from first data entry")] private Image SlothPic;
+    [SerializeField, Tooltip("Drag in text object that's underlined from fifth data entry")] private TMP_Text SlothName;
+    [SerializeField, Tooltip("Drag in text object that's next to the heart from fifth data entry")] private TMP_Text SlothHealth;
+    [SerializeField, Tooltip("Drag in text object that's next to the stomach from fifth data entry")] private TMP_Text SlothHunger;
+    [SerializeField, Tooltip("Drag in text object that's next to the smiley from fifth data entry")] private TMP_Text SlothHappy;
+    [SerializeField, Tooltip("Drag in slider object from fifth data entry")] private Slider SlothOverall;
+    [SerializeField, Tooltip("Drag in sliderfill object from fifth data entry to apply gradient")] private Image SlothFill;
+    [SerializeField, Tooltip("Drag in all sliders object from Sloth")] private Slider[] SlothCurrentStats;
     // Start is called before the first frame update
     void Start()
     {
@@ -93,18 +79,6 @@ public class StatTracker : MonoBehaviour
         MeerkatName.text = Names[3].text;
         SlothName.text = Names[4].text;
 
-        AnimalEntryList[0] = PenguinEntry;
-        AnimalEntryList[1] = PandaEntry;
-        AnimalEntryList[2] = CoatiEntry;
-        AnimalEntryList[3] = MeerkatEntry;
-        AnimalEntryList[4] = SlothEntry;
-
-        PriorityList[0] = PenguinPriority;
-        PriorityList[1] = PandaPriority;
-        PriorityList[2] = CoatiPriority;
-        PriorityList[3] = MeerkatPriority;
-        PriorityList[4] = SlothPriority;
-
     }
 
     // Update is called once per frame
@@ -116,7 +90,6 @@ public class StatTracker : MonoBehaviour
         PenguinHappy.text = ""+ PenguinCurrentStats[2].value;
         PenguinOverall.value =
             (PenguinCurrentStats[0].value + PenguinCurrentStats[1].value + PenguinCurrentStats[2].value)/3;
-        animalAverage[0] = PenguinOverall.value;
         PenguinFill.color = gradient.Evaluate(PenguinOverall.normalizedValue);
 
         PandaHealth.text = ""+ PandaCurrentStats[0].value;
@@ -124,7 +97,6 @@ public class StatTracker : MonoBehaviour
         PandaHappy.text = ""+ PandaCurrentStats[2].value;
         PandaOverall.value =
             (PandaCurrentStats[0].value + PandaCurrentStats[1].value + PandaCurrentStats[2].value)/3;
-        animalAverage[1] = PandaOverall.value;
         PandaFill.color = gradient.Evaluate(PandaOverall.normalizedValue);
         
         CoatiHealth.text = ""+ CoatiCurrentStats[0].value;
@@ -132,7 +104,6 @@ public class StatTracker : MonoBehaviour
         CoatiHappy.text = ""+ CoatiCurrentStats[2].value;
         CoatiOverall.value =
             (CoatiCurrentStats[0].value + CoatiCurrentStats[1].value + CoatiCurrentStats[2].value)/3;
-        animalAverage[2] = CoatiOverall.value;
         CoatiFill.color = gradient.Evaluate(CoatiOverall.normalizedValue);
         
         MeerkatHealth.text = ""+ MeerkatCurrentStats[0].value;
@@ -140,7 +111,6 @@ public class StatTracker : MonoBehaviour
         MeerkatHappy.text = ""+ MeerkatCurrentStats[2].value;
         MeerkatOverall.value =
             (MeerkatCurrentStats[0].value + MeerkatCurrentStats[1].value + MeerkatCurrentStats[2].value)/3;
-        animalAverage[3] = MeerkatOverall.value;
         MeerkatFill.color = gradient.Evaluate(MeerkatOverall.normalizedValue);
         
         SlothHealth.text = ""+ SlothCurrentStats[0].value;
@@ -148,10 +118,9 @@ public class StatTracker : MonoBehaviour
         SlothHappy.text = ""+ SlothCurrentStats[2].value;
         SlothOverall.value =
             (SlothCurrentStats[0].value + SlothCurrentStats[1].value + SlothCurrentStats[2].value)/3;
-        animalAverage[4] = SlothOverall.value;
         SlothFill.color = gradient.Evaluate(SlothOverall.normalizedValue);
 
-        for (int i = 0; i < animalAverage.Count; i++)
+        /*for (int i = 0; i < animalAverage.Count; i++)
         {
             for (int j = i+1; j < animalAverage.Count; j++) 
             {
@@ -160,7 +129,7 @@ public class StatTracker : MonoBehaviour
                     (animalAverage[i], animalAverage[j]) = (animalAverage[j], animalAverage[i]);
                 }
             }
-        }
+        }*/
     }
     
     /*IEnumerator ScrollLoop(Scrollbar vertScroll)
