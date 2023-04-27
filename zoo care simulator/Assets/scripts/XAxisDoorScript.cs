1ef43f319ec899ddef00e785486d79fa59953228
Ryan Hughes <ryan.hughes31@hotmail.com>
2023-04-27 23:22:56 +0100

    script reset

--- zoo care simulator/Assets/scripts/XAxisDoorScript.cs
+++ zoo care simulator/Assets/scripts/XAxisDoorScript.cs
@@ -1,29 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorScript : MonoBehaviour
+public class XAxisDoorScript : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private Animator[] DoorAnims;
     // Start is called before the first frame update
-
-    private void OnTriggerEnter(Collider other)
+    void Start()
     {
-        for (int i = 0; i < DoorAnims.Length; i++)
-        {
-            if (player.transform.position.x > this.transform.position.x)
-            {
-                DoorAnims[i].SetTrigger("GreaterThan");
-                Debug.Log("Greater x" + i);
-            }
-            else if (player.transform.position.x < this.transform.position.x)
-            {
-                DoorAnims[i].SetTrigger("LessThan");
-                Debug.Log("Lesser x" + i);
-            }
-            Debug.Log(player.transform.position.x + ", " + player.transform.position.z);
-        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
