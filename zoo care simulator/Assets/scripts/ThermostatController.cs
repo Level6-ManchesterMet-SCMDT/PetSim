38d458af869042683263a7c64bb9986ef02912eb
Ryan Hughes <ryan.hughes31@hotmail.com>
2023-05-01 20:28:32 +0100

    Thermostat created and coded. Need to add raycast button click

---
+++ zoo care simulator/Assets/scripts/ThermostatController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ThermostatController : MonoBehaviour
+{
+    [SerializeField] private TMP_Text TempDisplay;
+    public int startTemp;
+    private int currentTemp;
+    public int targetTemp;
+    public int minTemp;
+    public int maxTemp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTemp = startTemp;
+        TempDisplay.text = currentTemp + "°c";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseTemp()
+    {
+        if (currentTemp<maxTemp)
+        {
+            currentTemp += 1;
+        }
+        TempDisplay.text = currentTemp + "°c";
+        if (currentTemp == targetTemp)
+        {
+            Debug.Log("Target Temp Reached. Task Complete");
+        }
+    }
+    public void DecreaseTemp()
+    {
+        if (currentTemp>minTemp)
+        {
+            currentTemp -= 1;
+        }
+        TempDisplay.text = currentTemp + "°c";
+        if (currentTemp == targetTemp)
+        {
+            Debug.Log("Target Temp Reached. Task Complete");
+        }
+    }
+}
